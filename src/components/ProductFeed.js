cac7877b63c19fe9ac114d64d6b695ce0426e0e4
D. Aaren San <denis.san@swissdentalsolutions.com>
2021-05-21 01:36:52 +0200

    feat: add product and button

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -2,18 +2,50 @@ import Product from "./Product";
 
 const ProductFeed = ({ products }) => {
   return (
-    <div>
-      <h1>Products here...</h1>
-      {products.map(({ id, title, price, description, category, image }) => (
-        <Product
-          key={id}
-          title={title}
-          price={price}
-          description={description}
-          category={category}
-          image={image}
-        />
-      ))}
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
+      {products
+        .slice(0, 4)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
+      <img
+        className="md:col-span-full"
+        src="/images/1110572_smb_gw_desktop_1500x300_lavolio_1x_uk._CB484123630_.jpg"
+        alt=""
+      />
+      <div className="md:col-span-2">
+        {products
+          .slice(4, 5)
+          .map(({ id, title, price, description, category, image }) => (
+            <Product
+              key={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+            />
+          ))}
+      </div>
+      {products
+        .slice(5, products.length)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
     </div>
   );
 };
