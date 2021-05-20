010b1975f0958fd61b4e5636cfd189cfa5e9544d
D. Aaren San <denis.san@swissdentalsolutions.com>
2021-05-20 23:49:12 +0200

    feat: add banner

---
+++ src/components/Banner.js
@@ -0,0 +1,42 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const Banner = () => {
+  return (
+    <div className="relative">
+      <div className="absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-50" />
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={5000}
+      >
+        <div>
+          <img
+            loading="lazy"
+            src="/images/0504-AMZN-GNBC-GatewayHero-1500x600_v5._CB669739807_.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            loading="lazy"
+            src="/images/UGRR_S1_GWBleedingHero_ENG_COVIDUPDATE_XSite_1500X600_PV_en-GB._CB669781769_.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            loading="lazy"
+            src="/images/UK-EN_030821_SpringSitewide_ACQ_GW_Hero_D_1500x600_CV69._CB656397523_.jpg"
+            alt=""
+          />
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,22 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
       {/* Header */}
       <Header />
 
-      {/* Banner */}
+      <main className="max-w-screen-2xl mx-auto">
+        {/* Banner */}
+        <Banner />
 
-      {/* Product Feed */}
+        {/* Product Feed */}
+      </main>
     </div>
   );
 }
