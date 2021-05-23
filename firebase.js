ff11936cd5901fe6d6249af1562b4e52fed574f5
D. Aaren San <denis.san@swissdentalsolutions.com>
2021-05-23 11:58:41 +0200

    feat: add next auth and firebase

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDLbri93P14CGUpgrC_QOucldtqfV4-nuE",
+  authDomain: "amzn-yt-clone.firebaseapp.com",
+  projectId: "amzn-yt-clone",
+  storageBucket: "amzn-yt-clone.appspot.com",
+  messagingSenderId: "395021470332",
+  appId: "1:395021470332:web:c43bb812a16b5f61e117f9",
+};
