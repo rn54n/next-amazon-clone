929b417cff391ac2d9c65c52434066a54a8ca063
D. Aaren San <denis.san@swissdentalsolutions.com>
2021-05-23 17:51:40 +0200

    feat: add basket functionallity add and remove from basket

--- src/slices/basketSlice.js
+++ src/slices/basketSlice.js
@@ -8,11 +8,35 @@ export const basketSlice = createSlice({
   name: "basket",
   initialState,
   reducers: {
-    addToBasket: (state, action) => {},
-    removeFromBasket: (state, action) => {},
+    // Actions
+    addToBasket: (state, action) => {
+      state.items = [...state.items, action.payload];
+    },
+    removeFromBasket: (state, action) => {
+      // find the item
+      const index = state.items.findIndex(
+        (basketItem) => basketItem.id === action.payload.id
+      );
+
+      // make copy of current basket and change it
+      let newBasket = [...state.items];
+
+      // if it finds item to be removed it would be greater than 0
+      if (index >= 0) {
+        // The item exists in basket .... remove it
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Cannot remove product (id: ${action.payload.id}) as its not in the basket`
+        );
+      }
+      // reassigning the item of the global store to that basket
+      state.items = newBasket;
+    },
   },
 });
 
+// use throughout rest of application
 export const { addToBasket, removeFromBasket } = basketSlice.actions;
 
 // Selectors - This is how we pull information from the Global store slice
